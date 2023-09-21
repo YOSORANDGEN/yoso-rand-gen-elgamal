e4194ca264f0edda5e30ebc9fd4f4824ac966582
YOSORANDGEN <145658794+YOSORANDGEN@users.noreply.github.com>
2023-09-21 10:54:10 +0200

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused_imports)]
+#![allow(warnings, unused)]
+
+mod polynomials;
+mod randomness_generator;
+use randomness_generator::{RandomnessGenerator, PubParams};
+
+fn main() {
+    let t: u64 = 1;
+
+    let pp = PubParams {t: t, n: 2*t + 1, n_parties_total: 5*t+4};
+    let execution_leaks = false;
+    //let vss: VSS<Fq> = VSS { secret: 1.into(), pp: pp, execution_leaks: execution_leaks};
+    //vss.execute();
+
+    let rand_generator = RandomnessGenerator { pp: pp, execution_leaks: execution_leaks}; 
+    rand_generator.execute(); 
+
+    
+}
